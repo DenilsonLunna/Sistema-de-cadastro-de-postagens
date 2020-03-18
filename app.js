304737fc973f072d1e53e7f11ce04040e5f1946c
Denilson Sousa <denilsonsousa7@outlook.com>
2020-03-18 15:45:33 -0300

    Handlebars, Sequelize e conexão com banco de dados mySQL

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
 const app = express();
+const handlebars = require("express-handlebars");
+const Sequelize = require('sequelize');
+
+
+
+//Config
+    //Template engine
+    app.engine("handlebars", handlebars({defaultLayout:'main'}))
+    app.set("view engine", "handlebars");
+
+    //Conexão com banco de dados mySQL
+    const sequelize = new Sequelize('teste', 'root', '391223', {
+        host:"localhost",
+        dialect:"mysql"
+    })
 
 
 
 
-
+    
 app.listen(8081, function(){
     console.log("Servidor rodando");
 })
