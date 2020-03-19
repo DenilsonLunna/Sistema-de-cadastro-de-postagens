3735d880a37593a903e8dd4e8745cb644f73ecad
Denilson Sousa <denilsonsousa7@outlook.com>
2020-03-19 17:36:05 -0300

    Comunicacao back end front end estabelecida

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 const Sequelize = require('sequelize');
 
 
 
 //Config
     //Template engine
-    app.engine("handlebars", handlebars({defaultLayout:'main'}))
-    app.set("view engine", "handlebars");
-
+        app.engine("handlebars", handlebars({defaultLayout:'main'}))
+        app.set("view engine", "handlebars");
+    //Body parser
+        app.use(bodyParser.urlencoded({extended:false}))
+        app.use(bodyParser.json())
     //Conexão com banco de dados mySQL
-    const sequelize = new Sequelize('teste', 'root', '391223', {
-        host:"localhost",
-        dialect:"mysql"
-    })
-
+        const sequelize = new Sequelize('teste', 'root', '391223', {
+            host:"localhost",
+            dialect:"mysql"
+        })
     app.get('/cad', function(req,res){
         res.render("formulario");
     })
     app.post('/add', function(req,res){
-        res.send("Formulario recebido");
+        res.send("texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo);
     })
-
-
-
-
     
 app.listen(8081, function(){
     console.log("Servidor rodando");
